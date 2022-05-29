bb4aba54d9b5f867309e8e3cf3bf0395f7547f91
Luca Campello <luca.campello@allianz.it>
2022-05-29 11:57:52 +0200

    added basic structure for profile and scrollbar;

--- src/app/mainpage/mainpage.module.ts
+++ src/app/mainpage/mainpage.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainpageComponent } from './mainpage.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileModule } from '../profile/profile.module';
 
 const routes: Routes = [
   { path: '', component: MainpageComponent }
@@ -11,7 +12,8 @@ const routes: Routes = [
   declarations: [MainpageComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    ProfileModule
   ]
 })
 export class MainpageModule { }
---
+++ src/app/profile/profile.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ProfileComponent } from './profile.component';
+
+@NgModule({
+  declarations: [
+    ProfileComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ProfileComponent
+  ]
+})
+export class ProfileModule { }
