84d83c898142acae5f9d7a6de8a33aacce0693b9
Luca Campello <luca.campello@allianz.it>
2022-05-30 15:40:54 +0200

    first version of education experience; tweaks needed;

--- src/app/experience/experience-subsection/experience-subsection.component.ts
+++ src/app/experience/experience-subsection/experience-subsection.component.ts
@@ -15,10 +15,12 @@ export class ExperienceSubsectionComponent implements OnInit {
 
   ngOnInit(): void {
     this.workExperience.push(this.eurisExperience);
+    this.education.push(this.uniPdSecondExperience, this.uniPdFirstExperience);
   }
 
   workExperience: ExperienceItemModel[] = [];
-  education = ['UniCz', 'UniPdLnC', 'UniPdCs', 'HighSchool'];
+  education: ExperienceItemModel[] = [];
+  // education = ['UniCz', 'UniPdLnC', 'UniPdCs', 'HighSchool'];
   // certifications = [];
 
   eurisExperience: ExperienceItemModel = {
@@ -27,6 +29,22 @@ export class ExperienceSubsectionComponent implements OnInit {
     title: 'Euris SpA',
     subtitle: '2019 - present',
     description: `Back-End Web Developer for a leading customer in the insurance industry from 2020 to date. <b>Developing</b> a BFF (Backend For Frontend) to migrate previously existent business logic to a new backend.<br>In charge of the <b>technical analysis</b> of many new features introduced.<br>The <b>technologies and languages</b> used mainly are <b>NodeJS, Typescript, Git</b>.<br><br>Front-End Web Developer for an important platform of banking services from 2019 until late 2020. <b>Development</b> of a number of front-end processes, together with <b>unit testing and documentation</b>.<br>Development of a private npm library to group common elements for the various applications requested.<br>The <b>technologies and languages</b> mainly used have been <b>Angular, Typescript, HTML, CSS, SCSS, Karma and Jasmine, Git.</b>`
+  }
+
+  uniPdSecondExperience: ExperienceItemModel = {
+    logo: '../../../assets/UniPDLogo.png',
+    isWideLogo: true,
+    title: 'Università degli Studi di Padova',
+    subtitle: '2014-2018',
+    description: 'Bachelor\'s Degree - Linguistic and Cultural Mediation'
+  }
+
+  uniPdFirstExperience: ExperienceItemModel = {
+    logo: '../../../assets/UniPDLogo.png',
+    isWideLogo: true,
+    title: 'Università degli Studi di Padova',
+    subtitle: '2013-2014',
+    description: 'Bachelor\'s Degree - Computer Science - Interrupted'
   }
 
 }
