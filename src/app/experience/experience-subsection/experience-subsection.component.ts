56350959dc284c2ab57e70b6a71ddb534e4f9446
Luca Campello <luca.campello@allianz.it>
2022-05-30 18:26:40 +0200

    Added Erasmus experience and some fixing;

--- src/app/experience/experience-subsection/experience-subsection.component.ts
+++ src/app/experience/experience-subsection/experience-subsection.component.ts
@@ -15,7 +15,7 @@ export class ExperienceSubsectionComponent implements OnInit {
 
   ngOnInit(): void {
     this.workExperience.push(this.eurisExperience);
-    this.education.push(this.uniPdSecondExperience, this.uniPdFirstExperience);
+    this.education.push(this.uniCzErasmus, this.uniPdSecondExperience, this.uniPdFirstExperience);
   }
 
   workExperience: ExperienceItemModel[] = [];
@@ -29,6 +29,14 @@ export class ExperienceSubsectionComponent implements OnInit {
     title: 'Euris SpA',
     subtitle: '2019 - present',
     description: `Back-End Web Developer for a leading customer in the insurance industry from 2020 to date. <b>Developing</b> a BFF (Backend For Frontend) to migrate previously existent business logic to a new backend.<br>In charge of the <b>technical analysis</b> of many new features introduced.<br>The <b>technologies and languages</b> used mainly are <b>NodeJS, Typescript, Git</b>.<br><br>Front-End Web Developer for an important platform of banking services from 2019 until late 2020. <b>Development</b> of a number of front-end processes, together with <b>unit testing and documentation</b>.<br>Development of a private npm library to group common elements for the various applications requested.<br>The <b>technologies and languages</b> mainly used have been <b>Angular, Typescript, HTML, CSS, SCSS, Karma and Jasmine, Git.</b>`
+  }
+
+  uniCzErasmus: ExperienceItemModel = {
+    logo: '../../../assets/UniCZLogo.png',
+    isWideLogo: true,
+    title: 'Univerzita Karlova V Praze',
+    subtitle: '2018',
+    description: 'Erasmus - Part of Bachelor\'s Degree'
   }
 
   uniPdSecondExperience: ExperienceItemModel = {
