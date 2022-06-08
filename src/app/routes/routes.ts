7460d563f10434d7bc7c008968df73fd15b4fe21
Luca Campello <luca.campello@allianz.it>
2022-06-08 15:30:16 +0200

    fixed route path;

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from "@angular/router";
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'main', pathMatch: 'full' },
     { 
-        path: 'main',
+        path: '',
         loadChildren: () => import('../mainpage/mainpage.module').then(m => m.MainpageModule)
     },
-    { path: '**', redirectTo: 'main' }
+    { path: '**', redirectTo: '' }
 ];
